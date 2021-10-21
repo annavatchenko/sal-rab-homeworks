e1f436d18e28b57263443599d3ba45f9ac559650
annavatchenko <a.vatchenko@yandex.ru>
2021-10-21 23:48:22 +0300

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,6 +5,12 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
+    let shippingSum;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    } else  {
+        shippingSum = shippingPrice;
+    }
     // создайте переменную shippingSum
 
     // если productsSum равно 0,
@@ -27,6 +33,11 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
+    let discountSum;
+     if (productsSum >= discountMinSum) {
+         (discountSum = discountPart / 100 * productsSum);
+     } else {(discountSum = 0);
+    }
 
     // создайте переменную discountSum
 
@@ -45,6 +56,10 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
+
     // создайте переменную totalSum
 
     // присвойте totalSum значение productsSum
@@ -53,6 +68,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
+    totalSum = totalSum + shippingSum;
+    let freeShipping;
+    freeShipping = !shippingSum;
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
